00ef3d111324356cc58f57419bdb1630740abb52
Fernando Juarez <frenandister@gmail.com>
2018-01-03 20:26:13 -0600

    routes + giving an error

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var db = mongoose.connect("mongodb://localhost/bookAPI");
+
+var Book = require("./models/bookModel");
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 var port = process.env.PORT || 3000;
 
+var bookRouter = require("./Routes/bookRoutes")(Book);
+
+app.use("/api/books",bookRouter);
+//app.use("/api/authors",authorRouter);
+
 app.get("/", function(req,res){
-    res.send("helldddddo lobas");
+    res.send("heldddddo lobas");
 });
 
 app.listen(port, function(){
