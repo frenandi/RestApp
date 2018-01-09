b8371c6e0f3e632e859251c24eab6875af7c3c1c
frenandi <frenandister@gmail.com>
2018-01-09 06:48:27 -0600

    adding the remove

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -67,6 +67,14 @@ var routes = function(Book){
                 else
                     res.json(req.book);
             });
+        })
+        .delete(function(req,res){
+            req.book.remove(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else
+                    res.status(204).send("Removed");
+            });
         });
     return bookRouter;
 };
