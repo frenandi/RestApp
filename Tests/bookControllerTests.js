a916ae041d2f657eb4eeb05fa8ebb0c62fa14ee4
frenandi <frenandister@gmail.com>
2018-01-10 07:33:42 -0600

    adding the test for mocha and the dependencies

--- Tests/bookControllerTests.js
+++ Tests/bookControllerTests.js
@@ -1,8 +1,27 @@
 var should = require("should");
-var sinon = require("sinon");
+var sinon = require("sinon"); //sinon is to mock
 
 describe("Book Controller Tests:", function(){
     describe("Post", function(){
-        it("should not allow an empty tittle on post")
+        it("should not allow an empty tittle on post", function(){
+            var Book = function(book){
+                this.save = function(){}
+            };
+            var req = {
+                body: {
+                    author: "Fer"
+                }
+            };
+            var res = {
+                status: sinon.spy(),
+                send: sinon.spy()
+            };
+
+            var bookController = require("../controllers/bookController")(Book);
+            bookController.post(req,res);
+            console.log("this-----------------------"+res.status.args[0])
+            res.status.calledWith(400).should.equal(true, "Bad Status "+ res.status.args[0]);
+            res.send.calledWith("Title is required").should.equal(true);
+        })
     })
 })
