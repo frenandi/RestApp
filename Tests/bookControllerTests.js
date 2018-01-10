9ab8b9c62e1079870f85d5e5a5bbc3d59b0931ae
Fernando Juarez <frenandister@gmail.com>
2018-01-09 21:26:58 -0600

    Starting with mocha

---
+++ Tests/bookControllerTests.js
@@ -0,0 +1,8 @@
+var should = require("should");
+var sinon = require("sinon");
+
+describe("Book Controller Tests:", function(){
+    describe("Post", function(){
+        it("should not allow an empty tittle on post")
+    })
+})
